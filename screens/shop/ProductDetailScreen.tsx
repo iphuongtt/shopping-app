8c47bce24a5148f01d9af1829d774bc72169282f
Trinh Thanh Phuong <iphuongtt@gmail.com>
2020-01-18 14:15:38 +0700

    Working on the product detail screen

--- screens/shop/ProductDetailScreen.tsx
+++ screens/shop/ProductDetailScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, Image, Button, StyleSheet} from 'react-native';
 import {NavigationStackScreenComponent} from 'react-navigation-stack';
 
 import {useTypedSelector} from '../../store/reducers';
+import {colors} from '../../constants';
 
 export const ProductDetailScreen: NavigationStackScreenComponent = props => {
   const productId = props.navigation.getParam('productId');
@@ -10,9 +11,14 @@ export const ProductDetailScreen: NavigationStackScreenComponent = props => {
     state.products.availableProducts.find(product => product.id === productId),
   );
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{uri: selectedProduct.imageUrl}} />
+      <View style={styles.actions}>
+        <Button color={colors.primary} title="Add To Cart" onPress={() => {}} />
+      </View>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
@@ -21,5 +27,28 @@ ProductDetailScreen.navigationOptions = ({navigation}) => {
     headerTitle: navigation.getParam('productTitle'),
   };
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20,
+  },
+  button: {},
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+});
 
 export default ProductDetailScreen;
